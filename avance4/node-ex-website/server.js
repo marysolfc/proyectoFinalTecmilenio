95333b3de7bf87bbf7bb58835f3d02bbd5baa4af
Marisol FC <marisolfc@unam.mx>
2023-06-12 21:40:56 -0600

    Se comenta el código del avance 4

--- avance4/node-ex-website/server.js
+++ avance4/node-ex-website/server.js
@@ -1,13 +1,17 @@
+// Se importan los módulos para generar un servidor
 const http = require("http");
 const express = require("express");
 const path = require("path");
+
+// se crea una instancia de express para poder generar el servidor
 const app = express();
-app.use(express.json());
-app.use(express.static("express")); // default URL for website
-app.use("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/express/index.html"));
-  //__dirname : It will resolve to your project folder.
+app.use(express.json()); 
+app.use(express.static("express")); // esta es la url por default para el sitio web
+app.use("/", function (req, res) { // se define la ruta raiz del sitio web
+  res.sendFile(path.join(__dirname + "/express/index.html")); // se indica la union de la ruta principal con la ruta de algúna otr app  
 });
+
+// se indican los parámetros de configuración del servidor
 const server = http.createServer(app);
 const port = 3000;
 server.listen(port);
